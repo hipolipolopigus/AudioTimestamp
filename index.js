02ded428e2a76e39c84aac56b9bacf624d8f2ac8
Morgan Foley <hipolipolopigus@gmail.com>
2014-12-30 15:26:27 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+﻿var audioTimestamp;
+(function (audioTimestamp) {
+    audioTimestamp.player;
+    audioTimestamp.playerQuery = function () {
+        return $('#wrapper > audio');
+    };
+
+    audioTimestamp.timestampQuery = function () {
+        return $('#timestamp');
+    };
+    audioTimestamp.fileQuery = function () {
+        return $('#file');
+    };
+
+    audioTimestamp.currentFile;
+    audioTimestamp.timestamp;
+
+    $(function () {
+        var query = audioTimestamp.playerQuery();
+        audioTimestamp.player = query[0];
+
+        query.on('timeupdate', function () {
+            update(audioTimestamp.currentFile, audioTimestamp.timestamp = audioTimestamp.player.currentTime);
+        });
+
+        $('#timestamp-go').click(function () {
+            play(audioTimestamp.currentFile = audioTimestamp.fileQuery().val(), parseFloat(audioTimestamp.timestampQuery().val() || '0'));
+        });
+
+        $('#output-copy').on('mouseleave', function () {
+            return $('#output-copy').tooltip('hide');
+        });
+
+        audioTimestamp.currentFile = getParameterByName('file');
+        if (audioTimestamp.currentFile) {
+            console.log('Loading file:', audioTimestamp.currentFile);
+            audioTimestamp.fileQuery().attr('value', audioTimestamp.currentFile);
+        } else
+            console.log('No file supplied');
+        audioTimestamp.timestamp = 0;
+        try  {
+            audioTimestamp.timestamp = parseFloat(getParameterByName('time'));
+            $('#timestamp').attr('value', audioTimestamp.timestamp);
+        } catch (ex) {
+        }
+
+        if (audioTimestamp.currentFile)
+            play(audioTimestamp.currentFile, audioTimestamp.timestamp);
+    });
+
+    function update(file, timestamp) {
+        $('#output').attr('value', location['origin'] + location.pathname + '?file=' + file + '&time=' + timestamp);
+    }
+    audioTimestamp.update = update;
+
+    function play(file, timestamp) {
+        if (typeof timestamp === "undefined") { timestamp = 0; }
+        audioTimestamp.currentFile = file;
+        audioTimestamp.player.pause();
+        var query = audioTimestamp.playerQuery();
+        query.children('source').remove();
+        query.append($('<source/>').attr('src', file));
+        audioTimestamp.player.sourceIndex = 0;
+        audioTimestamp.player.currentTime = timestamp;
+        audioTimestamp.player.play();
+    }
+    audioTimestamp.play = play;
+
+    //http://stackoverflow.com/a/901144
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+})(audioTimestamp || (audioTimestamp = {}));
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,62 @@
+﻿module audioTimestamp {
+    export var player: HTMLAudioElement;
+    export var playerQuery = () => $('#wrapper > audio');
+
+    export var timestampQuery = () => $('#timestamp');
+    export var fileQuery = () => $('#file');
+
+    export var currentFile;
+    export var timestamp;
+
+    $(() => {
+        var query = playerQuery();
+        player = <any>query[0];
+
+        query.on('timeupdate', () => {
+            update(currentFile, timestamp = player.currentTime);
+        });
+
+        $('#timestamp-go').click(() => {
+            play(currentFile = fileQuery().val(), parseFloat(timestampQuery().val() ||'0'));
+        });
+
+        $('#output-copy').on('mouseleave', () => $('#output-copy').tooltip('hide'));
+
+        currentFile = getParameterByName('file');
+        if (currentFile) {
+            console.log('Loading file:', currentFile);
+            fileQuery().attr('value', currentFile);
+        }
+        else console.log('No file supplied');
+        timestamp = 0;
+        try {
+            timestamp = parseFloat(getParameterByName('time'));
+            $('#timestamp').attr('value', timestamp);
+        } catch (ex) {}
+
+        if (currentFile) play(currentFile, timestamp);
+    });
+
+    export function update(file: string, timestamp: number) {
+        $('#output').attr('value', location['origin'] + location.pathname + '?file=' + file + '&time=' + timestamp);
+    }
+
+    export function play(file: string, timestamp: number = 0) {
+        currentFile = file;
+        player.pause();
+        var query = playerQuery();
+        query.children('source').remove();
+        query.append($('<source/>').attr('src', file));
+        player.sourceIndex = 0;
+        player.currentTime = timestamp;
+        player.play();
+    }
+
+    //http://stackoverflow.com/a/901144
+    function getParameterByName(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+}
